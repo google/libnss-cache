b94f8ee9032e41b938527d86985feb7f775f04f3
Jamie Wilkinson <jaq@spacepants.org>
2014-10-21 16:36:54 +1100

    Add test case for the last entry errno bug.

---
+++ test/last_pw_errno_test.c
@@ -0,0 +1,19 @@
+#include <sys/types.h>
+#include <pwd.h>
+#include <errno.h>
+#include <stdio.h>
+
+int main() {
+  setpwent();
+  for (;;) {
+    errno = 0;
+    struct passwd *pw = getpwent();
+    if (!pw) {
+      if (errno != 0)
+        printf("ERRNO: %u %m\n", errno);
+      break;
+    }
+  }
+  endpwent();
+  return 0;
+}
